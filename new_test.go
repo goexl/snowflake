eec6dd381f66e3fd8f7fd3c6702bf7253374c706
storezhang <storezhang@gmail.com>
2025-12-07 20:32:47 +0800

    refactor(构建器): Epoch -> Base

--- new_test.go
+++ new_test.go
@@ -1,16 +1,20 @@
 package snowflake_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/goexl/snowflake"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNext(t *testing.T) {
-	id, err := snowflake.New().Build().Next()
+	fmt.Println(time.Now().UTC().Hour())
+	id, err := snowflake.New().Epoch(time.Date(2025, time.December, 7, 12, 31, 23, 12, time.UTC)).Build().Next()
 	assert.Nil(t, err)
 	assert.NotNil(t, id)
+	fmt.Println(id.Get(), ": ", id.Time())
 }
 
 func BenchmarkNext(b *testing.B) {
